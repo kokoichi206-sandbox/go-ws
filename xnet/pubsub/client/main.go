0861e8b10f2c4427e702006608d0b2c817da6ab3
kokoichi206 <tt.300607@gmail.com>
2024-04-20 02:46:24 +0900

    add: ベース作成

---
+++ xnet/pubsub/client/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+const (
+	hostPort     = "localhost:12345"
+	pingInterval = 3 * time.Second
+)
+
+var pingMessage = websocket.Codec{
+	Marshal:   marshal,
+	Unmarshal: unmarshal,
+}
+
+func marshal(_ any) (msg []byte, payloadType byte, err error) {
+	return []byte("ping"), websocket.PingFrame, nil
+}
+
+func unmarshal(msg []byte, payloadType byte, v any) (err error) {
+	return json.Unmarshal(msg, v)
+}
+
+func main() {
+	origin := fmt.Sprintf("http://%s", hostPort)
+	url := fmt.Sprintf("ws://%s/subscribe", hostPort)
+
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		fmt.Println("---------- write ping  ----------")
+
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("panic recovered: ", r)
+			}
+		}()
+
+		for range time.Tick(pingInterval) {
+			if err := pingMessage.Send(ws, nil); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	go func() {
+		fmt.Println("---------- read loop ----------")
+
+		for {
+			fr, err := ws.NewFrameReader()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			switch fr.PayloadType() {
+			case websocket.PongFrame:
+				b, _ := io.ReadAll(fr)
+				fmt.Printf("PongFrame string(b): %v\n", string(b))
+				continue
+
+			case websocket.TextFrame:
+				b, _ := io.ReadAll(fr)
+				fmt.Printf("string(b): %v\n", string(b))
+				continue
+				// fmt.Printf("Received: %s.\n", string(msg))
+			}
+		}
+	}()
+
+	for {
+		if _, err := ws.Write([]byte("test subscribe")); err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
---
+++ xnet/pubsub/server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+const (
+	hostPort = ":12345"
+)
+
+var pongMessage = websocket.Codec{
+	Marshal:   marshal,
+	Unmarshal: unmarshal,
+}
+
+func marshal(v any) (msg []byte, payloadType byte, err error) {
+	return []byte("thanks to ping!"), websocket.PongFrame, nil
+}
+
+func unmarshal(msg []byte, payloadType byte, v any) (err error) {
+	return json.Unmarshal(msg, v)
+}
+
+type textFR interface {
+	io.Reader
+	Len() int
+}
+
+func subscribe(ws *websocket.Conn) {
+	defer ws.Close()
+
+	for {
+		r, err := ws.NewFrameReader()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Printf("connection closed\n")
+				break
+			}
+
+			fmt.Printf("unexpected err: %v\n", err)
+		}
+
+		switch r.PayloadType() {
+		case websocket.PingFrame:
+			b, _ := io.ReadAll(r)
+			fmt.Printf("string(b): %v\n", string(b))
+			pongMessage.Send(ws, nil)
+			continue
+
+		case websocket.TextFrame:
+			handleTextFrame(r, ws)
+		}
+	}
+}
+
+func handleTextFrame(r textFR, ws *websocket.Conn) error {
+	if r.Len() > 1998_0206 {
+		return fmt.Errorf("too large payload: %d", r.Len())
+	}
+
+	res, err := io.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("failed to read payload: %w", err)
+	}
+
+	fmt.Printf("received: %s\n", res)
+	ws.Write(res)
+
+	return nil
+}
+
+func main() {
+	http.Handle("/subscribe", websocket.Handler(subscribe))
+	if err := http.ListenAndServe(hostPort, nil); err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
