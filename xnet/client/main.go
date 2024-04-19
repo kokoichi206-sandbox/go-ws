5189fccd967195a4624fbcad46c1a90a4e597561
kokoichi206 <tt.300607@gmail.com>
2024-04-20 01:18:16 +0900

    xnet での ping 追加

--- xnet/client/main.go
+++ xnet/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -10,16 +11,54 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+const (
+	hostPort = "localhost:12345"
+)
+
+var pingMessage = websocket.Codec{
+	Marshal:   marshal,
+	Unmarshal: unmarshal,
+}
+
+func marshal(_ any) (msg []byte, payloadType byte, err error) {
+	return []byte("ping"), websocket.PingFrame, nil
+}
+
+func unmarshal(msg []byte, payloadType byte, v any) (err error) {
+	return json.Unmarshal(msg, v)
+}
+
 func main() {
-	origin := "http://localhost:11111"
-	url := "ws://localhost:11111/subscribe"
-	// url := "ws://localhost:11111"
+	origin := fmt.Sprintf("http://%s", hostPort)
+	url := fmt.Sprintf("ws://%s/subscribe", hostPort)
+
 	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	err = pingMessage.Send(ws, nil)
+	fmt.Printf("err: %v\n", err)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("panic recovered: ", r)
+			}
+		}()
+
+		for range time.Tick(3 * time.Second) {
+			if err := pingMessage.Send(ws, nil); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
 	for {
+		if _, err := ws.Write([]byte("test subscribe")); err != nil {
+			log.Fatal(err)
+		}
+
 		fmt.Println("---------- read ----------")
 		ws.SetReadDeadline(time.Now().Add(10 * time.Second))
 		var msg = make([]byte, 512)
@@ -34,5 +73,7 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Printf("Received: %s.\n", msg[:n])
+
+		time.Sleep(5 * time.Second)
 	}
 }
